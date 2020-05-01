2663b0652915e251d65c260fe1e74fa74a999897
hirokichi1992 <plastar14105@gmail.com>
2020-05-01 18:46:45 +0900

    スプレッド構文

--- js/main2.js
+++ js/main2.js
@@ -1,5 +1,7 @@
 'use strict'; {
-    const scores = [90, 80, 100];
+    // スプレッド構文→「...」を前につけることで配列に配列を追加できる（つけないとarrayの状態で追加されてしまう）
+    const otherScores = [10, 50, 10];
+    const scores = [90, 80, 100, ...otherScores];
     // console.log(scores[0]);
 
     // constで定義した配列でも配列内要素に対しての変更は可能
@@ -15,7 +17,13 @@
 
 
     // 配列の途中に要素を追加または削除する→splice('開始位置','削除する個数','追加する要素');
-    scores.splice(1, 1, 30, 59);
+    // scores.splice(1, 1, 30, 59);
+
+    function sum(a, b) {
+        return a + b;
+    }
+
+    console.log(sum(...otherScores));
 
     // 配列の要素数は'length'で取得→配列の増減に対応できる
     for (let i = 0; i < scores.length; i++) {
