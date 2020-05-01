aff5a3a3faebf582f5bc0e2302026194b3f181ed
hirokichi1992 <plastar14105@gmail.com>
2020-05-01 17:50:32 +0900

    splice-配列の途中に要素を追加or削除

--- js/main2.js
+++ js/main2.js
@@ -8,10 +8,14 @@
     // console.log(scores);
 
     // 末尾に追加
-    scores.push(40, 60);
+    // scores.push(40, 60);
     // 先頭から削除
-    scores.shift();
+    // scores.shift();
     // 80, 100, 40, 60
+
+
+    // 配列の途中に要素を追加または削除する→splice('開始位置','削除する個数','追加する要素');
+    scores.splice(1, 1, 30, 59);
 
     // 配列の要素数は'length'で取得→配列の増減に対応できる
     for (let i = 0; i < scores.length; i++) {
