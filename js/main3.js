27ac9542c632d7fcd1b2e3595fe193bb85dc7e70
hirokichi1992 <plastar14105@gmail.com>
2020-05-04 07:45:09 +0900

    オブジェクト要素の変更・追加・削除

--- js/main3.js
+++ js/main3.js
@@ -3,5 +3,18 @@
         x: 100,
         y: 200,
     };
+
+    // オブジェクトの参照
+    // console.log(point.x);
+    // console.log(point['y']);
+
+    // オブジェクト要素の変更
+    // point.x = 120;
+    // point['y'] = 900;
+    // console.log(point);
+
+    // オブジェクト要素の追加・削除
+    point.z = 80;
+    delete point.x;
     console.log(point);
 }
