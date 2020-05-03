1f96a150ff0b4cfa77fd39483c26568a2736a077
hirokichi1992 <plastar14105@gmail.com>
2020-05-04 07:54:20 +0900

    スプレッド構文・分割代入・レスト構文

--- js/main3.js
+++ js/main3.js
@@ -1,8 +1,18 @@
 'use strict'; {
+
+    const otherProps = {
+        r: 100,
+        color: 'blue',
+    };
+
     const point = {
         x: 100,
         y: 200,
+        ...otherProps, // スプレッド構文:「...」を前につけることで配列に配列を追加できる
     };
+
+    // 分割代入・レスト構文:：...othersにはpointの残りのオブジェクトが格納される
+    const { x, r, ...others } = point;
 
     // オブジェクトの参照
     // console.log(point.x);
@@ -14,7 +24,12 @@
     // console.log(point);
 
     // オブジェクト要素の追加・削除
-    point.z = 80;
-    delete point.x;
+    // point.z = 80;
+    // delete point.x;
+    // console.log(point);
+
     console.log(point);
+    console.log(x);
+    console.log(r);
+    console.log(others);
 }
