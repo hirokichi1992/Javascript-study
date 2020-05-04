5a079d022b6b681b6b8742a5c6d23103e869820d
hirokichi1992 <plastar14105@gmail.com>
2020-05-04 17:50:11 +0900

    alert, confirm

---
+++ js/timer.js
@@ -0,0 +1,9 @@
+'use strict'; {
+    // アラート
+    alert('yahoo');
+
+    // 削除する時の確認時に使用する
+    const answer = confirm('削除しますか？');
+    answer === true ? console.log('削除しました') : console.log('キャンセルしました');
+
+}
