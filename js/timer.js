61e864c0c4ba8dbb9a59ddefe2c50eaf03fd751f
hirokichi1992 <plastar14105@gmail.com>
2020-05-04 17:58:29 +0900

    setInterval, clearInterval

--- js/timer.js
+++ js/timer.js
@@ -1,9 +1,25 @@
 'use strict'; {
     // アラート
-    alert('yahoo');
+    // alert('yahoo');
 
     // 削除する時の確認時に使用する
-    const answer = confirm('削除しますか？');
-    answer === true ? console.log('削除しました') : console.log('キャンセルしました');
+    // const answer = confirm('削除しますか？');
+    // answer === true ? console.log('削除しました') : console.log('キャンセルしました');
+
+    // カウンタ
+    let i = 0;
+
+    // 時間を表示する関数
+    function showTime() {
+        console.log(new Date);
+        i++;
+        if (i > 2) {
+            // clearInterval: setIntervalを取り消す
+            clearInterval(IntervalId);
+        }
+    }
+
+    // setInterval(): showTime関数を1000ms毎に繰り返す
+    const IntervalId = setInterval(showTime, 1000);
 
 }
