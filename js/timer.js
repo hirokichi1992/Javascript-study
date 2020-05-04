05c263f663a61b3bc750b3d3a69977aeffcccad4
hirokichi1992 <plastar14105@gmail.com>
2020-05-05 08:01:12 +0900

    setTimeout()-繰り返し処理

--- js/timer.js
+++ js/timer.js
@@ -10,16 +10,40 @@
     let i = 0;
 
     // 時間を表示する関数
+    // function showTime() {
+    //     console.log(new Date);
+    //     i++;
+    //     if (i > 2) {
+    //         // clearInterval: setIntervalを取り消す
+    //         clearInterval(IntervalId);
+    //     }
+    // }
+
+    // setInterval(): showTime関数を1000ms毎に繰り返す
+    // const IntervalId = setInterval(showTime, 1000);
+
+
+    // setTimeout(): 指定時間後に1回だけ実行する
+
+    // function showTime() {
+    //     console.log(new Date);
+    // }
+
+    // setTimeout(showTime, 1000);
+
+
+    // setTimeoutを使った繰り返し処理方法
     function showTime() {
         console.log(new Date);
+        // 1s後にshowTime()を実行
+        const TimeoutId = setTimeout(showTime, 1000);
+        // カウンタ増分
         i++;
         if (i > 2) {
-            // clearInterval: setIntervalを取り消す
-            clearInterval(IntervalId);
+            // TimeoutIdを取り消し
+            clearTimeout(TimeoutId);
         }
     }
 
-    // setInterval(): showTime関数を1000ms毎に繰り返す
-    const IntervalId = setInterval(showTime, 1000);
-
+    showTime();
 }
