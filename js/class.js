aa5ab388c5444344bfc927504a5a923ae7bcb3e1
hirokichi1992 <plastar14105@gmail.com>
2020-05-05 10:56:44 +0900

    メソッド

--- js/class.js
+++ js/class.js
@@ -3,20 +3,25 @@
             name: 'ジャスミン',
             text: 'Javascriptのお勉強中',
             likeCount: 0,
+            show() {
+                console.log(`Name: ${this.name} post: ${this.text} ${this.likeCount}いいね`);
+            },
         },
         {
             name: 'ハーブ',
             text: 'githubで草を生やすことの意味について',
             likeCount: 0,
+            show() {
+                console.log(`Name: ${this.name} post: ${this.text} ${this.likeCount}いいね`);
+            },
         },
     ]
-
-    function show(post) {
-        console.log(`Name: ${post.name} post: ${post.text} ${post.likeCount}いいね`);
-    }
 
     // show(posts[0]); // Name: ジャスミン post: Javascriptのお勉強中 0いいね
     // posts.forEach((post) => {
     //     show(post);
     // }); // Name: ジャスミン post: Javascriptのお勉強中 0いいね Name: ハーブ post: githubで草を生やすことの意味について 0いいね
+
+    // メソッド呼び出し
+    posts[0].show();
 }
