44cc83ae92b8b795ad9fa0d04534643aefe025bb
hirokichi1992 <plastar14105@gmail.com>
2020-05-05 11:22:13 +0900

    クラス定義と表示

--- js/class.js
+++ js/class.js
@@ -1,20 +1,18 @@
 'use strict'; {
-    const posts = [{
-            name: 'ジャスミン',
-            text: 'Javascriptのお勉強中',
-            likeCount: 0,
-            show() {
-                console.log(`Name: ${this.name} post: ${this.text} ${this.likeCount}いいね`);
-            },
-        },
-        {
-            name: 'ハーブ',
-            text: 'githubで草を生やすことの意味について',
-            likeCount: 0,
-            show() {
-                console.log(`Name: ${this.name} post: ${this.text} ${this.likeCount}いいね`);
-            },
-        },
+    class Post {
+        constructor(name, text) {
+            this.name = name,
+                this.text = text,
+                this.likeCount = 0
+        }
+
+        show() {
+            console.log(`Name: ${this.name} post: ${this.text} ${this.likeCount}いいね`);
+        }
+    }
+    const posts = [
+        new Post('aso', '今日も日本の財政を良くするぞ'),
+        new Post('kono', '今日も日本の外交を良くするぞ'),
     ]
 
     // show(posts[0]); // Name: ジャスミン post: Javascriptのお勉強中 0いいね
