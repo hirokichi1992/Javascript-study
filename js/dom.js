a496c3fd700414cf85e87d5a295d8e65dda0fe4a
hirokichi1992 <plastar14105@gmail.com>
2020-05-05 17:06:57 +0900

    querySelectorAll: 全て取得

--- js/dom.js
+++ js/dom.js
@@ -2,7 +2,14 @@
     function change() {
         // document.querySelector('h1').textContent = 'changed word';
         // document.querySelector('#target').textContent = 'changed word';
-        document.getElementById('target').textContent = 'changed word';
+        // document.getElementById('target').textContent = 'changed word';
+        // document.querySelector('p').textContent = 'changed word'; // 1番目しか変更されない
+        // document.querySelectorAll('p')[2].textContent = 'changed word'; // 全て取得可能→配列のようにアクセス可能
+        // forEachで全取得したp要素のテキストを順番に変更する
+        document.querySelectorAll('p').forEach((p, num) => {
+            p.textContent = `${num}番目のp要素です`;
+        })
+
     }
 
     // 1000ms後に呼び出し
