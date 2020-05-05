653d0dfeae714f98a8bb46f25cb24661ca904995
hirokichi1992 <plastar14105@gmail.com>
2020-05-05 17:29:09 +0900

    title, style属性の操作

--- js/dom.js
+++ js/dom.js
@@ -11,9 +11,15 @@
     //     })
     // }
 
-    // ボタンを押したらテキストを変更する
+
+    // ボタンを押したら何らかの処理を実行する
     document.querySelector('button').addEventListener('click', () => {
-        document.getElementById('target').textContent = 'changed word';
+        const targetNode = document.getElementById('target');
+
+        targetNode.textContent = 'changed word'; // textの変更
+        targetNode.title = 'this is changed title from javascript'; // titleの変更
+        targetNode.style.color = "red"; // css colorの変更（非推奨-CSSとの役割が曖昧になるのでクラスのつけ外しによって制御する方法が推奨）
+        targetNode.style.background = "blue"; // css backgroundの変更（非推奨-CSSとの役割が曖昧になるのでクラスのつけ外しによって制御する方法が推奨）
     })
 
 }
