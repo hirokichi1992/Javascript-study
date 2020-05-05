3f5bb1a0ff964e630934dc06390b40e039969e5b
hirokichi1992 <plastar14105@gmail.com>
2020-05-05 11:56:01 +0900

    静的メソッド

--- js/class.js
+++ js/class.js
@@ -14,6 +14,11 @@
             this.likeCount++;
             this.show();
         }
+
+        // 静的メソッド→クラスから直接呼び出し可
+        static showInfo() {
+            console.log('静的メソッド内ではthisは使えない。理由はインスタンスからの呼び出しではない為。constructorで使用しているthisはインスタンス化されたオブジェクトのこと。');
+        }
     }
     const posts = [
         new Post('aso', '今日も日本の財政を良くするぞ'),
@@ -33,6 +38,11 @@
     // posts[0].likeCount++
     // posts[0].show();
 
+
     // 追加メソッド呼び出し
     posts[0].like();
+
+
+    // 静的メソッドの呼び出し
+    Post.showInfo();
 }
